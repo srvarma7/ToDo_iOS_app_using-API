166dff97b735ba57f9cfde4bec2c3a284d12608a
srvarma7 <srvarma7@gmail.com>
2020-07-28 15:51:58 +1000

    Addind project and server

---
+++ ToDo_iOS_app/ToDo_iOS_app/ToDoVC.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ToDo_iOS_app
+//
+//  Created by Sai Raghu Varma Kallepalli on 28/7/20.
+//  Copyright © 2020 Sai Raghu Varma Kallepalli. All rights reserved.
+//
+
+import UIKit
+
+class ToDoVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
