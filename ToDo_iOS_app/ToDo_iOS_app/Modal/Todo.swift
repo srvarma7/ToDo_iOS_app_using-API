dbfad1997337aec56aaaea0f47cbdfd2d1d0c1f9
srvarma7 <srvarma7@gmail.com>
2020-07-28 18:56:30 +1000

    Adding struct to hold JSON response

---
+++ ToDo_iOS_app/ToDo_iOS_app/Modal/Todo.swift
@@ -0,0 +1,18 @@
+//
+//  Todo.swift
+//  ToDo_iOS_app
+//
+//  Created by Sai Raghu Varma Kallepalli on 28/7/20.
+//  Copyright © 2020 Sai Raghu Varma Kallepalli. All rights reserved.
+//
+
+import Foundation
+
+struct ToDo: Codable {
+    var item: String
+    var priority: Int
+}
+
+struct APIError: Codable {
+    var message: String
+}
